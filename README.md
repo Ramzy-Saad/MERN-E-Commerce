11d1014a4dac422146a61e9c1bb2e91e6fa0474e
Ramzy Saad <www.ramzysaad16@gmail.com>
2025-07-30 16:42:35 +0300

    render product-List

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# React + TypeScript + Vite
+# React + TypeScript + Vite as an E-Commerce
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import { sampleProducts } from './types/data';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <header>Amazon excommerce</header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug} >
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              />
+              <h2>{product.name}</h2>
+              <p>$ {product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All rights for Ramzy</footer>
     </div>
   );
