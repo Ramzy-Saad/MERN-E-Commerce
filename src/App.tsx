26dc555bce9e4cd11c69e14733056d01372be059
Ramzy Saad <www.ramzysaad16@gmail.com>
2025-07-30 17:56:38 +0300

    Nav bar bootstrab

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import './App.css';
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
 import { sampleProducts } from './types/data';
 
 function App() {
   return (
-    <div>
-      <header>Amazon excommerce</header>
+    <div className="d-flex flex-column vh-100">
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand href="/">Ramzy's Ecommerce</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href='/cart' className='nav-link'>Cart</a>
+            <a href='/signin' className='nav-link'>Sign In</a>
+          </Nav>
+        </Navbar>
+      </header>
       <main>
-        <ul>
-          {sampleProducts.map((product) => (
-            <li key={product.slug} >
-              <img
-                src={product.image}
-                alt={product.name}
-                className="product-image"
-              />
-              <h2>{product.name}</h2>
-              <p>$ {product.price}</p>
-            </li>
-          ))}
-        </ul>
+        <Container className='mt-3'>
+          <Row>
+            {sampleProducts.map((product) => (
+              <Col key={product.slug} sm={6} md={3} lg={4} >
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="product-image"
+                />
+                <h2>{product.name}</h2>
+                <p>$ {product.price}</p>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </main>
-      <footer>All rights for Ramzy</footer>
+      <footer>
+        <div className='text-center'>
+          All rights for Ramzy
+        </div>
+        </footer>
     </div>
   );
 }
