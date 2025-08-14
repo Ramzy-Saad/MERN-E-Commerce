cea551a8fe32abbc71aaa8e912bb90971c428c9c
Ramzy Saad <www.ramzysaad16@gmail.com>
2025-08-14 12:13:04 +0300

    fetch products

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,4 @@
-import type { ApiError } from "./types/apiError";
-
+import type { ApiError } from "./types/ApiError"
 export const getError = (error:ApiError) =>{
     return error.response && error.response.data.message
     ?error.response.data.message
